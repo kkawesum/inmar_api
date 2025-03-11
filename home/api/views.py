6c838e484074a884746e0781c17ceffdc609e3d7
kislay_s <kk.ism07@gmail.com>
2025-03-11 14:59:41 +0530

    updated subcategory view

--- home/api/views.py
+++ home/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .serializers import SubCategorySerializer, SkuSerializer, CategorySerializer, LocationSerializer,DepartmentSerializer
 from metadata.models import Sku,SubCategory,Category,Location,Department
 from django.db.models import Q
@@ -62,9 +63,10 @@ class cat_by_dept_loc(APIView):
             for category in categories:
                 if (category.department.id==dep_id):
                     q.append(category) 
+            products = Product.objects.select_related("category", "category__department").all()
 
             # cat = Category.objects.filter(department_id = dep_id)
-            print(q)
+            # print(q)
             serializer = CategorySerializer(q,many=True)
             return Response({
                 'data': serializer.data,
@@ -81,20 +83,15 @@ class cat_by_dept_loc(APIView):
 class Scat_cat_dep_loc(APIView):
      def get(self,request,loc_id,dep_id,cat_id):
         try:
-            #    print('77777777777777777')
-               loc = Location.objects.filter(id=loc_id)
-            #    print('till')
-               deps = Department.objects.select_related("location").all()
-            #    print(deps)
-            #    cats = Category.objects.filter(department_id=dep_id)
-               c = Category.objects.select_related('department').all()
-               
-               sub_cat = SubCategory.objects.select_related('category')
-               serializer = SubCategorySerializer(sub_cat,many=True)
-               return Response({
-                    'data': serializer.data,
-                    'message': 'data fetched successfully'
-                },status=status.HTTP_200_OK)
+            loc = Location.objects.filter(id=loc_id)
+            deps = Department.objects.select_related("location").all()
+            c = Category.objects.select_related('department').all()
+            sub_cat = SubCategory.objects.select_related('category')
+            serializer = SubCategorySerializer(sub_cat,many=True)
+            return Response({
+                'data': serializer.data,
+                'message': 'data fetched successfully'
+            },status=status.HTTP_200_OK)
         except Exception as e:
                     return Response({
                             'data': {},
